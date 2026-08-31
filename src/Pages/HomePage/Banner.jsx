30a92c2cea275ee225b39cf5b3c21e3d46b50f10
Srabonti <somayaaktarshabonti721@gmail.com>
2026-08-31 10:51:07 +0600

    install motion use to banner transition, rotate, ananimate, color

--- src/Pages/HomePage/Banner.jsx
+++ src/Pages/HomePage/Banner.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
-
+import { motion } from "motion/react";
 const Banner = () => {
     return (
-<div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img
-      alt="Tailwind CSS hero component"
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
+        <div className="hero bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                {/* <img src={team1} alt="" /> */}
+                <div>
+                    <motion.h1
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1, transition: { duration: 4 } }}
+                        className="text-5xl font-bold"
+                    >
+                        <motion.span
+                            animate={{
+                                color: ["#F59F27", "#27F568", "#F52757"],
+                                transition: { duration: 4, repeat: Infinity },
+                            }}
+                        >
+                            Remote jobs
+                        </motion.span>{" "}
+                        for you!
+                    </motion.h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                        assumenda excepturi exercitationem quasi. In deleniti
+                        eaque aut repudiandae et a id nisi.
+                    </p>
+
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
