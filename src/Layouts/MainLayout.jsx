a2d92245a47d9a6b9de0f92fda8079b5a7b6532b
Srabonti <somayaaktarshabonti721@gmail.com>
2026-09-01 09:21:28 +0600

    added banner use to motion image

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../Pages/Shared/Footer';
 
 const MainLayout = () => {
     return (
-        <div className='container mx-auto'>
+        <div className='max-w-7xl mx-auto'>
             <Header/>
             <Outlet/>
             <Footer/>
--- src/Pages/HomePage/Banner.jsx
+++ src/Pages/HomePage/Banner.jsx
@@ -1,10 +1,31 @@
 import { motion } from "motion/react";
+import team1 from "../../assets/developer/team1.jpg";
+import team2 from "../../assets/developer/team2.jpg";
 const Banner = () => {
     return (
-        <div className="hero bg-base-200">
+        <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                {/* <img src={team1} alt="" /> */}
-                <div>
+                <div className="flex-1">
+                    <motion.img
+                        animate={{ y: [20, 80, 20] }}
+                        transition={{ duration: 5, repeat: Infinity }}
+                        src={team1}
+                        className="max-w-sm border-blue-600 border-l-8 border-b-8 rounded-r-[40px]  rounded-tl-[40px] shadow-2xl"
+                        alt="team1"
+                    />
+                    <motion.img
+                        animate={{ x: [100, 150, 100] }}
+                        transition={{
+                            duration: 10,
+                            delay: 5,
+                            repeat: Infinity,
+                        }}
+                        src={team2}
+                        className="max-w-sm border-blue-600 border-l-8 border-b-8 rounded-r-[40px]  rounded-tl-[40px] shadow-2xl"
+                        alt="team2"
+                    />
+                </div>
+                <div className="flex-1">
                     <motion.h1
                         initial={{ scale: 0 }}
                         animate={{ scale: 1, transition: { duration: 4 } }}
